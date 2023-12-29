554588742eee166863c319bd988ae5887099753a
Соня <sonamarkova849@gmail.com>
2023-12-30 00:41:50 +0900

    я хочу прозрачный фон и кнопки нормальные

--- po.py
+++ po.py
@@ -10,6 +10,7 @@ pygame.init()
 width = 800
 height = 800
 screen = pygame.display.set_mode((width, height))
+COLOR_KOG = (3, 183, 172)
 
 
 def terminate():
@@ -45,11 +46,12 @@ class Button(pygame.sprite.Sprite):
         else:
             self.h = 95
         self.x, self.y = pos
-        image = load_image("кнопка.png")
+        image = load_image("кнопка.png", (255, 255, 255))
         self.image = pygame.transform.scale(image, (375, self.h))
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+        self.rect_for_text = (self.rect.x + 25, self.rect.y + 25, self.rect.width, self.rect.height)
         self.flag = flag
 
     def update(self):
@@ -101,40 +103,63 @@ def create_particles(position, all_sprites):
 
 def start_screen(screen, all_sprites):
     clock = pygame.time.Clock()
-    for i in range(10000):
-        screen.fill(pygame.Color('white'),
-                    (random.random() * width,
-                     random.random() * height, 1, 1))
     font = pygame.font.SysFont('Franklin Gothic', 50)
-    text = font.render('СТАРТ', True, (248, 244, 255))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screen, (248, 244, 255), (text_x - 10, text_y - 10,
-                                               text_w + 20, text_h + 20), 1)
+    start = font.render('СТАРТ', True, COLOR_KOG)
+    finish = font.render('ВЫХОД', True, COLOR_KOG)
+    name = font.render('Приложение для развития навыков при', True, COLOR_KOG)
+    name1 = font.render('подготовке к ЕГЭ по информатике', True, COLOR_KOG)
+    start_x = 235
+    start_y = 400
+    start_w = start.get_width()
+    start_h = start.get_height()
+    screen.blit(start, (start_x, start_y))
+    finish_x = 435
+    finish_y = 400
+    finish_w = finish.get_width()
+    finish_h = finish.get_height()
+    screen.blit(finish, (finish_x, finish_y))
+    name_x = width // 2 - name.get_width() // 2
+    name_y = height // 2 - name.get_height() // 2 - 175
+    screen.blit(name, (name_x, name_y))
+    name1_x = width // 2 - name1.get_width() // 2
+    name1_y = height // 2 - name1.get_height() // 2 - 105
+    screen.blit(name1, (name1_x, name1_y))
+    pygame.draw.rect(screen, COLOR_KOG, (start_x - 10, start_y - 10,
+                                         start_w + 20, start_h + 20), 5)
+    pygame.draw.rect(screen, COLOR_KOG, (finish_x - 10, finish_y - 10,
+                                         finish_w + 20, finish_h + 20), 5)
+    pygame.draw.rect(screen, COLOR_KOG, (35, 35,
+                                               width - 70, height - 70), 5)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
 
-            elif event.type == pygame.MOUSEBUTTONDOWN and text_x - 10 <= event.pos[0] <= (
-                    text_x + text_w + 20) and text_y - 10 <= event.pos[1] <= (
-                    text_y + text_h + 20):
+            elif event.type == pygame.MOUSEBUTTONDOWN and start_x - 10 <= event.pos[0] <= (
+                    start_x + start_w + 20) and start_y - 10 <= event.pos[1] <= (
+                    start_y + start_h + 20):
                 return
-            elif event.type == pygame.MOUSEBUTTONDOWN and not (text_x - 10 <= event.pos[0] <= (
-                    text_x + text_w + 20) and text_y - 10 <= event.pos[1] <= (
-                                                                       text_y + text_h + 20)):
+            elif event.type == pygame.MOUSEBUTTONDOWN and finish_x - 10 <= event.pos[0] <= (
+                    finish_x + finish_w + 20) and finish_y - 10 <= event.pos[1] <= (
+                    finish_y + finish_h + 20):
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN and not (start_x - 10 <= event.pos[0] <= (
+                    start_x + start_w + 20) and start_y - 10 <= event.pos[1] <= (
+                                                                       start_y + start_h + 20)):
                 create_particles(pygame.mouse.get_pos(), all_sprites)
         all_sprites.update()
-        screen.fill((0, 0, 0))
+        screen.fill((255, 255, 255))
         all_sprites.draw(screen)
-        screen.blit(text, (text_x, text_y))
-        pygame.draw.rect(screen, (248, 244, 255), (text_x - 10, text_y - 10,
-                                                   text_w + 20, text_h + 20), 1)
-        pygame.display.flip()
-        clock.tick(50)
+        screen.blit(name, (name_x, name_y))
+        screen.blit(name1, (name1_x, name1_y))
+        screen.blit(start, (start_x, start_y))
+        screen.blit(finish, (finish_x, finish_y))
+        pygame.draw.rect(screen, COLOR_KOG, (start_x - 10, start_y - 10,
+                                             start_w + 20, start_h + 20), 5)
+        pygame.draw.rect(screen, COLOR_KOG, (finish_x - 10, finish_y - 10,
+                                             finish_w + 20, finish_h + 20), 5)
+        pygame.draw.rect(screen, COLOR_KOG, (35, 35,
+                                             width - 70, height - 70), 5)
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -162,9 +187,18 @@ def main_window(screen, all_sprites, args):
 
     result = Button((205, 645), all_sprites, args[4])
     font = pygame.font.SysFont('Franklin Gothic', 60)
-    for line in intro_text:
-        string_rendered = font.render(line, True, (248, 244, 255))
-        screen.blit(string_rendered, strup.rect)
+    string_rendered1 = font.render(intro_text[0], True, (248, 244, 255))
+    screen.blit(string_rendered1, strup.rect_for_text)
+    string_rendered2 = font.render(intro_text[1], True, (248, 244, 255))
+    screen.blit(string_rendered2, schulte.rect_for_text)
+    string_rendered3 = font.render(intro_text[2], True, (248, 244, 255))
+    screen.blit(string_rendered3, table.rect_for_text)
+    string_rendered4 = font.render(intro_text[3], True, (248, 244, 255))
+    screen.blit(string_rendered4, cursor.rect_for_text)
+    string_rendered5 = font.render(intro_text[4], True, (248, 244, 255))
+    screen.blit(string_rendered5, cursor.rect_for_text)
+    string_rendered6 = font.render(intro_text[5], True, (248, 244, 255))
+    screen.blit(string_rendered6, result.rect_for_text)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -176,9 +210,18 @@ def main_window(screen, all_sprites, args):
         all_sprites.update()
         screen.blit(fon, (0, 0))
         all_sprites.draw(screen)
-        for line in intro_text:
-            string_rendered = font.render(line, True, (248, 244, 255))
-            screen.blit(string_rendered, (strup.rect.x, strup.rect.y * intro_text.index(line), strup.h, 375))
+        string_rendered1 = font.render(intro_text[0], True, (248, 244, 255))
+        screen.blit(string_rendered1, strup.rect_for_text)
+        string_rendered2 = font.render(intro_text[1], True, (248, 244, 255))
+        screen.blit(string_rendered2, schulte.rect_for_text)
+        string_rendered3 = font.render(intro_text[2], True, (248, 244, 255))
+        screen.blit(string_rendered3, table.rect_for_text)
+        string_rendered4 = font.render(intro_text[3], True, (248, 244, 255))
+        screen.blit(string_rendered4, cursor.rect_for_text)
+        string_rendered5 = font.render(intro_text[4], True, (248, 244, 255))
+        screen.blit(string_rendered5, cursor.rect_for_text)
+        string_rendered6 = font.render(intro_text[5], True, (248, 244, 255))
+        screen.blit(string_rendered6, result.rect_for_text)
         pygame.display.flip()
         clock.tick(FPS)
 
