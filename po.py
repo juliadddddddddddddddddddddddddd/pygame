d32e78b545de04a5d6d8732d4d35a87f633938ca
Соня <sonamarkova849@gmail.com>
2024-01-11 22:51:30 +0900

    с клиновидными таблицами

--- po.py
+++ po.py
@@ -26,6 +26,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 VIOLET = (105, 0, 198)
 ORANGE = (255, 102, 0)
+WHITE = (255, 255, 255)
 COLORS = [RED, YELLOW, GREEN, BLUE, VIOLET, ORANGE]
 COLOR_WORD = ['КРАСНЫЙ', 'ОРАНЖЕВЫЙ', 'СИНИЙ', 'ФИОЛЕТОВЫЙ', 'ЗЕЛЕНЫЙ', 'ЖЕЛТЫЙ']
 
@@ -82,20 +83,47 @@ def Schulte(screen):
 
 def Table(screen):
     clock = pygame.time.Clock()
-    screen.fill((255, 255, 255))
-    fon = pygame.transform.scale(load_image('когнетивные2.png'),
-                                 (width, height))
-    screen.blit(fon, (0, 0))
-    while True:
-        even_list = pygame.event.get()
-        for event in even_list:
+    num = RandomNumbersDisplay()
+    alpth = num.generate_random_numbers(10)
+    alpth1 = num.generate_additional_numbers(20)
+    running = True
+    while running:
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                terminate()
+                running = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 return
-        screen.blit(fon, (0, 0))
+        num.display_numbers(alpth, alpth1)
         pygame.display.flip()
         clock.tick(FPS)
+
+
+class RandomNumbersDisplay:
+    def __init__(self):
+        self.font = pygame.font.Font(None, 36)
+
+    def generate_random_numbers(self, count):
+        return [random.randint(1, 100) for _ in range(count)]
+
+    def generate_additional_numbers(self, add_count):
+        return [str(random.randint(1, 100)) for _ in range(add_count)]
+
+    def display_numbers(self, numbers, numbers1):
+        screen.fill((255, 255, 255))
+        y = 100  # Начальная координата Y для вывода чисел
+        x = 50
+        for i in range(len(numbers)):
+            text_surface = self.font.render(str(numbers[i]), True, (0, 0, 0))
+            text_rect = text_surface.get_rect(center=(width // 2, y))
+            screen.blit(text_surface, text_rect)
+            text_surface_x_left = self.font.render(str(numbers1[2 * i]), True, (0, 0, 0))
+            text_rect_x_left = text_surface.get_rect(center=(width // 2 + x, y))
+            screen.blit(text_surface_x_left, text_rect_x_left)
+            text_surface_x_right = self.font.render(str(numbers1[2 * i + 1]), True, (0, 0, 0))
+            text_rect_x_right = text_surface.get_rect(center=(width // 2 - x, y))
+            screen.blit(text_surface_x_right, text_rect_x_right)
+            y += 60  # Увеличение координаты Y для следующего числа
+            x += 20
 
 
 def Cursor(screen):
@@ -507,7 +535,6 @@ def final_window(screen):  # это окно нужно будет потом и
                 terminate()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 return
-
 
         screen.blit(fon, (0, 0))
         pygame.display.flip()
