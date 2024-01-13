aa3e8c766f706d6b2ee7dd36126954319fc18505
Соня <sonamarkova849@gmail.com>
2024-01-13 18:37:25 +0900

    11111

--- po.py
+++ po.py
@@ -38,13 +38,17 @@ def Strup(screen):
     stroop = StroopTest()
     stroop.draw_words(all_sprites)
     vol = 0.5
+    sec = 0
+    minu = 0
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                return
+                if sec < 10:
+                    return f'{minu}:0{sec}'
+                return f'{minu}:{sec}'
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 5:
                 vol -= 0.1
                 pygame.mixer.music.set_volume(vol)
@@ -54,6 +58,10 @@ def Strup(screen):
         screen.fill((0, 0, 0))
         all_sprites.draw(screen)
         pygame.display.flip()
+        time.sleep(1)
+        sec += 1
+        minu += sec // 60
+        sec = sec % 60
         clock.tick(FPS)
 
 
@@ -70,7 +78,9 @@ def Schulte(screen):
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                return
+                if sec < 10:
+                    return f'{minu}:0{sec}'
+                return f'{minu}:{sec}'
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 5:
                 vol -= 0.1
                 pygame.mixer.music.set_volume(vol)
@@ -80,12 +90,12 @@ def Schulte(screen):
         screen.fill((255, 255, 255))
         board.draw(screen)
 
-        pygame.draw.rect(screen, BLACK, (0, 0, 75, 35), 1)
+        pygame.draw.rect(screen, BLACK, (5, 5, 60, 30), 1)
         if sec < 10:
             time_text = font.render(f'{minu}:0{sec}', True, (0, 0, 0))
         else:
             time_text = font.render(f'{minu}:{sec}', True, (0, 0, 0))
-        screen.blit(time_text, (5, 5, 75, 35))
+        screen.blit(time_text, (10, 8, 60, 30))
 
         pygame.display.flip()
         time.sleep(1)
@@ -101,64 +111,27 @@ def Table(screen):
     alpth = num.generate_random_numbers(10)
     alpth1 = num.generate_additional_numbers(20)
     running = True
+    sec = 0
+    minu = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                return
+                if sec < 10:
+                    return f'{minu}:0{sec}'
+                return f'{minu}:{sec}'
         num.display_numbers(alpth, alpth1)
         pygame.display.flip()
+
+        time.sleep(1)
+        sec += 1
+        minu += sec // 60
+        sec = sec % 60
         clock.tick(FPS)
-
-
-class RandomNumbersDisplay:
-    def __init__(self):
-        self.font = pygame.font.Font(None, 36)
-
-    def generate_random_numbers(self, count):
-        return [random.randint(1, 100) for _ in range(count)]
-
-    def generate_additional_numbers(self, add_count):
-        return [str(random.randint(1, 100)) for _ in range(add_count)]
-
-    def display_numbers(self, numbers, numbers1):
-        screen.fill((255, 255, 255))
-        y = 100  # Начальная координата Y для вывода чисел
-        x = 50
-        for i in range(len(numbers)):
-            text_surface = self.font.render(str(numbers[i]), True, (0, 0, 0))
-            text_rect = text_surface.get_rect(center=(width // 2, y))
-            screen.blit(text_surface, text_rect)
-            text_surface_x_left = self.font.render(str(numbers1[2 * i]), True, (0, 0, 0))
-            text_rect_x_left = text_surface.get_rect(center=(width // 2 + x, y))
-            screen.blit(text_surface_x_left, text_rect_x_left)
-            text_surface_x_right = self.font.render(str(numbers1[2 * i + 1]), True, (0, 0, 0))
-            text_rect_x_right = text_surface.get_rect(center=(width // 2 - x, y))
-            screen.blit(text_surface_x_right, text_rect_x_right)
-            y += 60  # Увеличение координаты Y для следующего числа
-            x += 20
 
 
 def Cursor(screen):
-    clock = pygame.time.Clock()
-    screen.fill((255, 255, 255))
-    fon = pygame.transform.scale(load_image('когнетивные2.png'),
-                                 (width, height))
-    screen.blit(fon, (0, 0))
-    while True:
-        even_list = pygame.event.get()
-        for event in even_list:
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                return
-        screen.blit(fon, (0, 0))
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-def Result(screen):
     clock = pygame.time.Clock()
     screen.fill((255, 255, 255))
     fon = pygame.transform.scale(load_image('когнетивные2.png'),
@@ -195,6 +168,15 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+def create_particles(position, all_sprites):
+    # количество создаваемых частиц
+    particle_count = 20
+    # возможные скорости
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers), all_sprites)
 
 
 screen_rect = (0, 0, width, height)
@@ -251,6 +233,34 @@ class StroopTest:
             all_sprites.add(sprite)
 
 
+class RandomNumbersDisplay:
+    def __init__(self):
+        self.font = pygame.font.Font(None, 36)
+
+    def generate_random_numbers(self, count):
+        return [random.randint(1, 100) for _ in range(count)]
+
+    def generate_additional_numbers(self, add_count):
+        return [str(random.randint(1, 100)) for _ in range(add_count)]
+
+    def display_numbers(self, numbers, numbers1):
+        screen.fill((255, 255, 255))
+        y = 100  # Начальная координата Y для вывода чисел
+        x = 50
+        for i in range(len(numbers)):
+            text_surface = self.font.render(str(numbers[i]), True, (0, 0, 0))
+            text_rect = text_surface.get_rect(center=(width // 2, y))
+            screen.blit(text_surface, text_rect)
+            text_surface_x_left = self.font.render(str(numbers1[2 * i]), True, (0, 0, 0))
+            text_rect_x_left = text_surface.get_rect(center=(width // 2 + x, y))
+            screen.blit(text_surface_x_left, text_rect_x_left)
+            text_surface_x_right = self.font.render(str(numbers1[2 * i + 1]), True, (0, 0, 0))
+            text_rect_x_right = text_surface.get_rect(center=(width // 2 - x, y))
+            screen.blit(text_surface_x_right, text_rect_x_right)
+            y += 60  # Увеличение координаты Y для следующего числа
+            x += 20
+
+
 class Board:
     def __init__(self, cell_size):
         self.cell_size = cell_size
@@ -299,22 +309,13 @@ class Particle(pygame.sprite.Sprite):
             self.kill()
 
 
-def create_particles(position, all_sprites):
-    # количество создаваемых частиц
-    particle_count = 20
-    # возможные скорости
-    numbers = range(-5, 6)
-    for _ in range(particle_count):
-        Particle(position, random.choice(numbers), random.choice(numbers), all_sprites)
-
-
 def start_screen(screen, all_sprites):
     clock = pygame.time.Clock()
     vol = 0.5
     font = pygame.font.Font(font_name, 37)
     start1 = font.render(' Начать', True, BLACK)
-    finish1 = font.render('Выйти', True, BLACK)
     start2 = font.render(' Начать', True, BLACK, (179, 233, 230))
+    finish1 = font.render('Выйти', True, BLACK)
     finish2 = font.render('Выйти', True, BLACK, (179, 233, 230))
     name = font.render('Приложение для развития навыков при', True, BLACK)
     name1 = font.render('подготовке к ЕГЭ по информатике', True, BLACK)
@@ -411,51 +412,41 @@ def main_window(screen):
     intro_text = ["Тест Струпа",
                   "Таблицы Шульте",
                   "Клиновидные таблицы",
-                  "Курсор как инструмент",
-                  "для поддержания внимания",
-                  "Результат"]
+                  "Курсор"]
     fon = pygame.transform.scale(load_image('когнетивные2.png'),
                                  (width, height))
     screen.blit(fon, (0, 0))
     nota = pygame.transform.scale(load_image('нота.png'),
                                   (45, 45))
     screen.blit(nota, (0, 0))
-    font = pygame.font.Font(font_name, 42)
+    font = pygame.font.Font(font_name, 55)
     strup1 = font.render(intro_text[0], True, BLACK)
     schulte1 = font.render(intro_text[1], True, BLACK)
     table1 = font.render(intro_text[2], True, BLACK)
     cursor11 = font.render(intro_text[3], True, BLACK)
-    cursor12 = font.render(intro_text[4], True, BLACK)
-    result1 = font.render(intro_text[5], True, BLACK)
 
     flag = False
     touch = False
 
-    strup = Button((screen.get_width() // 2 - strup1.get_width() // 2, 150), flag, touch,
+    strup = Button((screen.get_width() // 2 - strup1.get_width() // 2, 175), flag, touch,
                    (strup1.get_height(), strup1.get_width()))
 
-    schulte = Button((screen.get_width() // 2 - schulte1.get_width() // 2, 250), flag, touch,
+    schulte = Button((screen.get_width() // 2 - schulte1.get_width() // 2, 300), flag, touch,
                      (schulte1.get_height(), schulte1.get_width()))
 
-    table = Button((screen.get_width() // 2 - table1.get_width() // 2, 350), flag, touch,
+    table = Button((screen.get_width() // 2 - table1.get_width() // 2, 425), flag, touch,
                    (table1.get_height(), table1.get_width()))
 
-    cursor = Button((screen.get_width() // 2 - cursor12.get_width() // 2, 450), flag, touch,
-                    (cursor11.get_height() + cursor12.get_height(),
-                     cursor12.get_width()))
-
-    result = Button((screen.get_width() // 2 - result1.get_width() // 2, 595), flag, touch,
-                    (result1.get_height(), result1.get_width()))
+    cursor = Button((screen.get_width() // 2 - cursor11.get_width() // 2, 550), flag, touch,
+                    (cursor11.get_height(),
+                     cursor11.get_width()))
 
     music = Button((0, 0), flag, touch, (45, 45))
     screen.blit(strup1, strup.rect_for_text)
     screen.blit(schulte1, schulte.rect_for_text)
     screen.blit(table1, table.rect_for_text)
     screen.blit(cursor11, cursor.rect_for_text)
-    screen.blit(cursor12, (
-        cursor.rect_for_text[0], cursor.rect_for_text[1] + 95, cursor.rect_for_text[2], cursor.rect_for_text[3]))
-    screen.blit(result1, result.rect_for_text)
-    group = pygame.sprite.Group(strup, schulte, table, cursor, result)
+    group = pygame.sprite.Group(strup, schulte, table, cursor)
     while True:
         even_list = pygame.event.get()
         for event in even_list:
@@ -470,25 +461,21 @@ def main_window(screen):
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 group.update(even_list)
                 if strup.flag:
-                    Strup(screen)
                     final_window(screen)
+                    Strup(screen)
                     strup.flag = False
                 elif schulte.flag:
-                    Schulte(screen)
                     final_window(screen)
+                    Schulte(screen)
                     schulte.flag = False
                 elif cursor.flag:
-                    Cursor(screen)
                     final_window(screen)
+                    Cursor(screen)
                     cursor.flag = False
                 elif table.flag:
+                    final_window(screen)
                     Table(screen)
-                    final_window(screen)
                     table.flag = False
-                elif result.flag:
-                    Result(screen)
-                    final_window(screen)
-                    result.flag = False
                 elif event.type == pygame.MOUSEBUTTONDOWN and music.x <= event.pos[0] <= (
                         music.x + music.w) and music.y <= event.pos[1] <= (
                         music.y + music.h):
@@ -508,49 +495,56 @@ def main_window(screen):
         schulte2 = font.render(intro_text[1], True, BLACK, (179, 233, 230))
         table2 = font.render(intro_text[2], True, BLACK, (179, 233, 230))
         cursor21 = font.render(intro_text[3], True, BLACK, (179, 233, 230))
-        cursor22 = font.render(intro_text[4], True, BLACK, (179, 233, 230))
-        result2 = font.render(intro_text[5], True, BLACK, (179, 233, 230))
 
         if strup.touch:
-
             screen.blit(strup2, strup.rect_for_text)
         elif schulte.touch:
             screen.blit(schulte2, schulte.rect_for_text)
         elif cursor.touch:
             screen.blit(cursor21, cursor.rect_for_text)
-            screen.blit(cursor22, (
-                cursor.rect_for_text[0], cursor.rect_for_text[1] + 38, cursor.rect_for_text[2],
-                cursor.rect_for_text[3]))
         elif table.touch:
             screen.blit(table2, table.rect_for_text)
-        elif result.touch:
-            screen.blit(result2, result.rect_for_text)
 
         screen.blit(strup1, strup.rect_for_text)
         screen.blit(schulte1, schulte.rect_for_text)
         screen.blit(table1, table.rect_for_text)
         screen.blit(cursor11, cursor.rect_for_text)
-        screen.blit(cursor12, (
-            cursor.rect_for_text[0], cursor.rect_for_text[1] + 38, cursor.rect_for_text[2], cursor.rect_for_text[3]))
-        screen.blit(result1, result.rect_for_text)
         pygame.display.flip()
         clock.tick(FPS)
 
 
 def final_window(screen):  # это окно нужно будет потом изменить (возможно здесь как раз таки и выводить результаты)
     clock = pygame.time.Clock()
-    fon = pygame.transform.scale(load_image('когнетивные.png'),
+    fon = pygame.transform.scale(load_image('когнетивные2.png'),
                                  (width, height))
+    font = pygame.font.Font(font_name, 37)
+    start1 = font.render(' Начать', True, BLACK)
+    start2 = font.render(' Начать', True, BLACK, (179, 233, 230))
+    res = font.render('Предыдущие результаты', True, BLACK)
     screen.blit(fon, (0, 0))
+
+    flag = False
+    touch = False
+    start_btn = Button((screen.get_width() // 2 - start1.get_width() // 2 - 15, 650), flag, touch,
+                       (start1.get_height(), start1.get_width()))
     while True:
         even_list = pygame.event.get()
         for event in even_list:
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                return
-
+                start_btn.update(even_list)
+                if start_btn.flag:
+                    return
+            elif event.type == pygame.MOUSEMOTION:
+                start_btn.update(even_list)
+                pass
         screen.blit(fon, (0, 0))
+        screen.blit(res, (screen.get_width() // 2 - res.get_width() // 2 , 100, res.get_height(), res.get_width()))
+        screen.blit(start1, start_btn.rect_for_text)
+        if start_btn.touch:
+            screen.blit(start2, start_btn.rect_for_text)
+
         pygame.display.flip()
         clock.tick(FPS)
 
